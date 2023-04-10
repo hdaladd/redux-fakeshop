4d62ea6d2a2685c2bb28a4601e0eb1b2da0d456f
Hannah <hdaladd@icloud.com>
2023-04-10 17:24:07 -0400

    updated redux fakeshop

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductComponent from "./ProductCompenent";
+import axios from "axios";
+import { useEffect } from "react";
+import { setProducts } from "../redux/actions/productActions";
+
+const ProductListing = () => {
+     const products = useSelector((state) => state);
+     const dispatch = useDispatch();
+
+    const fetchProducts = async () => {
+      const response = await axios.get('https://fakestoreapi.com/products').catch((err) => {
+                console.log("Err", err);
+            });
+        dispatch(setProducts(response.data));
+};
+
+useEffect(() => {
+    fetchProducts();
+}, []);
+console.log("Products: ", products);
+return (
+  <div className="ui grid container">
+    <ProductComponent />
+  </div>
+     );
+};
+export default ProductListing;
